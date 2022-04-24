44b018dfecc4e06087c74d5e40433ed8e570a5e6
admin <chakradhar06@gmail.com>
2022-04-23 23:23:21 -0400

    terra_output

--- 04-ec2/ec2.tf
+++ 04-ec2/ec2.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 resource "aws_instance" "frontend" {
-  ami           = "ami-005e54dee72cc1d00" # us-west-2
+  ami           = "ami-0bb6af715826253bf" # us-west-2
   instance_type = "t2.micro"
   vpc_security_group_ids = [aws_security_group.allow_tls.id]
   tags = {
