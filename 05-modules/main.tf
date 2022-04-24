3106b5dcaf59aef53d0da5538cebe3de76f89a9f
admin <chakradhar06@gmail.com>
2022-04-24 12:01:19 -0400

    terra_output

--- 05-modules/main.tf
+++ 05-modules/main.tf
@@ -1,3 +1,20 @@
+module "ec2" {
+  source = "./ec2"
+  sg_id = module.sg.sg_id
+}
+
+module "sg" {
+  source = "./sg"
+}
+
+terraform {
+  backend "s3" {
+    bucket = "terraform-chakra"
+    key    = "example/terraform.tfstate"
+    region = "us-east-1"
+  }
+}
+
 provider "aws" {
   region = "us-east-1"
 }
