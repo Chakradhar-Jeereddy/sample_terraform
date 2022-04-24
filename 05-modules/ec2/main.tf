df220ec45a72b1e66477150fe0eb128779dcaed2
admin <chakradhar06@gmail.com>
2022-04-24 12:37:39 -0400

    terra_output

--- 05-modules/ec2/main.tf
+++ 05-modules/ec2/main.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "frontend" {
-  ami                    = data.aws_ami.ami.image_id
+  ami                    = "ami-0bb6af715826253bf"
   instance_type = "t2.micro"
   vpc_security_group_ids = [var.sg_id]
   tags = {
