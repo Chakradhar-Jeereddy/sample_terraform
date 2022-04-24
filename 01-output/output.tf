403afc5221f218c00d0bb800efe321f4116985b8
admin <chakradhar06@gmail.com>
2022-04-23 22:32:50 -0400

    terra_output

--- 01-output/output.tf
+++ 01-output/output.tf
@@ -1,35 +1,3 @@
-variable "sample1" {
-  default = "hello"
-}
-
 output "sample1" {
-  value = var.sample1
-}
-
-#variable type
-variable "sample2" {
-  type = string
-  default = "hello"
-}
-
-output "sample2" {
-  value = var.sample2
-}
-
-variable "sample3" {
-  type = number
-  default = 100
-}
-
-output "sample3" {
-  value = var.sample3
-}
-
-variable "sample4" {
-  type = bool
-  default = true
-}
-
-output "sample4" {
-  value = var.sample4
+  value = "hello world"
 }
--- 02-vars/vars.tf
+++ 02-vars/vars.tf
@@ -2,11 +2,34 @@ variable "sample1" {
   default = "hello"
 }
 
-output "sample" {
+output "sample1" {
   value = var.sample1
 }
 
-#type of variables
+#variable type
 variable "sample2" {
+  type = string
+  default = "hello"
+}
+
+output "sample2" {
+  value = var.sample2
+}
+
+variable "sample3" {
+  type = number
   default = 100
+}
+
+output "sample3" {
+  value = var.sample3
+}
+
+variable "sample4" {
+  type = bool
+  default = true
+}
+
+output "sample4" {
+  value = var.sample4
 }
